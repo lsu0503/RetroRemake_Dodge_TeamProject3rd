1d2deaad27da098649ce14334aa2fd7531862f3b
gwanyoungkim719 <0000kgy@naver.com>
2024-10-17 20:58:30 +0900

    Update Movement
    name change

--- RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Projectile/ProjectileMovement.cs
+++ RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Projectile/ProjectileMovement.cs
@@ -1,29 +1,37 @@
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class ProjectileOnShoot : MonoBehaviour
+public class ProjectileMovement : MonoBehaviour
 {
     private Rigidbody2D projectileRigidbody;
-    private ProjectileData data;
+    [SerializeField] private ProjectileData data;
     private TrailRenderer trailRenderer;
 
     private float currentDuration;
 
+
     private void Awake()
     {
         projectileRigidbody = GetComponent<Rigidbody2D>();
-        data = GetComponent<ProjectileData>();
         trailRenderer = GetComponent<TrailRenderer>();
+        currentDuration = 0;
+        data = GetComponent<ProjectileData>();
 
     }
 
 
     private void Update()
     {
+        currentDuration += Time.deltaTime;
+
         if (currentDuration > data.duration)
         {
-            gameObject.SetActive(false);
+            ObjectPool.Instance.gameObject.SetActive(false);
         }
         projectileRigidbody.velocity = gameObject.transform.right * data.speed;
+        //발사각도 
+        //
     }
+
+
 }
