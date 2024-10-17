4e39e9786ea07cee2b0312843226baef1a14278d
Lookandme <kgg9386@gmail.com>
2024-10-17 11:32:30 +0900

    [Add] Add ScoreManager

---
+++ RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Manager/ScoreManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+
+    public int player1Score { get; private set; }
+    public int player2Score { get; private set; }
+
+    List<float[]> DamageRatio = new List<float[]>();  
+
+
+    private void Awake()
+    {
+        instance = this;
+        if (instance == null)
+        {
+            instance = this;
+            
+        }
+        else if (instance != this) 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public int IndividualScore(int playerNum, int amount)
+    {
+        switch (playerNum)
+        {
+            case 0:
+
+                player1Score += amount;
+                return player1Score;
+
+            case 1:
+                player2Score += amount;
+                return player2Score;
+
+            default:
+                return -1;
+
+        } 
+    }
+    public void AddDamage()
+    {
+        
+    }
+}
