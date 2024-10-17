1ab4c6234e97d4e73d4eb9d564a07c111b25b546
gwanyoungkim719 <0000kgy@naver.com>
2024-10-17 15:32:08 +0900

    Add ObjectPool

---
+++ RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Util/ObjectPool.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+   public class Pool
+    {
+        public string nameTag;
+        public GameObject prefab;
+        public int poolSize;
+        
+    }
+
+    public List<Pool> pools = new List<Pool>();
+    public Dictionary<string, Queue<GameObject>> PoolDictionary;
+
+    private void Awake()
+    {
+        PoolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach(var pool in pools)
+        {
+            Queue<GameObject> queue = new Queue<GameObject>();
+            for(int i =0; i < pool.poolSize; i++)
+            {
+                GameObject projectile = Instantiate(pool.prefab);
+                projectile.SetActive(false);
+                queue.Enqueue(projectile);
+            }
+            PoolDictionary.Add(pool.nameTag, queue);
+        }
+        
+    }
+
+    public GameObject SpawnFromPool(string tag)
+    {
+        if (!PoolDictionary.ContainsKey(tag))
+        {
+            return null;
+        }
+        
+        GameObject obj = PoolDictionary[tag].Dequeue();
+        PoolDictionary[tag].Enqueue(obj);
+
+        obj.SetActive(true);
+        return obj;
+
+
+    }
+
+}
