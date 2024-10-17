0a949e3cd2cb565b8c5b517fc889843d6d336e36
Lookandme <kgg9386@gmail.com>
2024-10-17 19:36:21 +0900

    [Update] Button → ResultUiButton

--- RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Manager/ResultUiButton.cs
+++ RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Manager/ResultUiButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Button : MonoBehaviour
+public class ResultUiButton : MonoBehaviour
 {
     public void ReturnTitile()
     {
