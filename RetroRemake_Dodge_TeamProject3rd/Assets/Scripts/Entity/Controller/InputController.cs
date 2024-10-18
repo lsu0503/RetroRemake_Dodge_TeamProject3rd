0491315e8e2bd113de27b391f65bccf885e8531e
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-10-18 20:31:15 +0900

    [Update] Improve Controll System

--- RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Entity/Controller/InputController.cs
+++ RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Entity/Controller/InputController.cs
@@ -3,15 +3,42 @@ using UnityEngine.InputSystem;
 
 public class InputController : Controller
 {
+    [SerializeField] private int playerNum;
+    private PlayerInput playerInput;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    private void Start()
+    {
+        if(playerNum == 0)
+        {
+            if (!GameManager.Instance.isMultiplay)
+                playerInput.SwitchCurrentActionMap("SinglePlayer");
+            else
+                playerInput.SwitchCurrentActionMap("Player1");
+        }
+
+        else if(playerNum == 1)
+        {
+            if (!GameManager.Instance.isMultiplay)
+                Destroy(gameObject);
+            else
+                playerInput.SwitchCurrentActionMap("Player2");
+        }
+    }
+
     public void OnMove(InputValue value)
     {
         Vector2 moveInput = value.Get<Vector2>().normalized;
-
         CallMoveEvent(moveInput);
     }
 
     public void OnAttack(InputValue value)
     {
         isAttacking = value.isPressed;
+        Debug.Log(isAttacking);
     }
 }
