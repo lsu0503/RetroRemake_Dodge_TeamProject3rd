25f089e2ae4442127486946a43b7e6313fb75284
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-10-18 14:50:51 +0900

    [Add] Character Movement System

---
+++ RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Entity/Behavior/Movement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Controller controller;
+    private Rigidbody2D rigid;
+
+    [SerializeField] float velocity = 8.0f;
+    
+
+    private Vector2 movementDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        controller = GetComponent<Controller>();
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(movementDirection);
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        Vector2 movement = direction * velocity;
+
+        rigid.velocity = movement;
+        float posY = Mathf.Clamp(transform.position.y, -5.5f, 6.0f);
+        float posX = Mathf.Clamp(transform.position.x, -13.5f, 13.5f);
+        transform.position = new Vector2(posX, posY);
+    }
+}
---
+++ RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Entity/Controller/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public event Action<Vector2> OnMoveEvent;
+    public event Action OnAttackEvent;
+    
+    protected bool isAttacking { get; set; }
+    private float timeSinceLastAttack = float.MaxValue;
+    [SerializeField] protected float timeToNextAttack = 0.5f;
+    
+    private void HandleAttackDelay()
+    {
+        if(timeSinceLastAttack < timeToNextAttack)
+        {
+            timeSinceLastAttack += Time.deltaTime;
+        }
+
+        else if (isAttacking)
+        {
+            timeSinceLastAttack = 0.0f;
+            CallAttackEvent();
+        }
+    }
+
+    public void CallMoveEvent(Vector2 direction)
+    {
+        OnMoveEvent?.Invoke(direction);
+    }
+
+    private void CallAttackEvent()
+    {
+        OnAttackEvent?.Invoke();
+    }
+}
