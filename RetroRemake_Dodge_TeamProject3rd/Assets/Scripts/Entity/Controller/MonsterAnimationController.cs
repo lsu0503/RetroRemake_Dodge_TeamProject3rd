8e8721eb4d27d4e8b04cdb262e1a343ca27dff6d
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-10-21 03:22:32 +0900

    [Update] Improve Some Systems
    Change Color of Boss Health Bar's lost gauge
    Improve Enemy Animation Controller

--- RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Entity/Controller/MonsterAnimationController.cs
+++ RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Entity/Controller/MonsterAnimationController.cs
@@ -4,6 +4,12 @@ public class MonsterAnimationController : CharacterAnimationController
 {
     private static readonly int isAttack = Animator.StringToHash("isAttack");
 
+    protected override void Start()
+    {
+        base.Start();
+        controller.OnAttackEvent += Attack;
+    }
+
     private void Attack()
     {
         animator.SetTrigger(isAttack);
