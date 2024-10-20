1ad562a099589a191e4dfa939d889efc3eac0c10
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-10-20 20:43:36 +0900

    [Add] Monster Dictionary System

---
+++ RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Entity/MonsterDictionary.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterDictionary : MonoBehaviour
+{
+    public Dictionary<string, GameObject> monsterDict;
+    
+    public GameObject InstanciateItem(string monsterName)
+    {
+        if (!monsterDict.ContainsKey(monsterName))
+            return null;
+
+        GameObject spawnMonster = Instantiate(monsterDict[monsterName]);
+        spawnMonster.SetActive(false);
+        return spawnMonster;
+    }
+}
