76ba0bc47055aa2c4f58f0783dc382cc387c7d44
gwanyoungkim719 <0000kgy@naver.com>
2024-10-20 13:43:45 +0900

    [Add] Item Collider

---
+++ RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Item/ItemCollider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class ItemCollider : MonoBehaviour
+{
+    private ItemData itemData;
+    private void Awake()
+    {
+        itemData = GetComponent<ItemData>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (itemData.itemType == 0)
+            {
+                ItemManager itemManager = GetComponent<ItemManager>();
+                itemManager.ItemLife();
+            }
+            else if (itemData.itemType == 1)
+            {
+                ItemManager itemManager = GetComponent<ItemManager>();
+                itemManager.ItemBomb();
+            }
+            else 
+            {
+                ItemManager itemManager = GetComponent<ItemManager>();
+                itemManager.ItemScore();
+            }
+        }
+    }
+}
