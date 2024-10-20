1c82516b9bfb95a26b1716adb5e5a45e6aebb846
gwanyoungkim719 <0000kgy@naver.com>
2024-10-20 23:26:02 +0900

    [Add] Magnetic Effect
    & Tag Item , Layer Item , Magnetic

---
+++ RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Entity/Behavior/MagneticEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MagneticEffect : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Item"))
+        {
+            Vector2 distance = transform.position - collision.transform.position;
+            collision.GetComponent<Rigidbody2D>().AddForce(distance.normalized * speed,ForceMode2D.Impulse);
+        }
+    }
+}
