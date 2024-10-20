5b7f1a5713b900f6d3c7265337333ed91a01a90d
gwanyoungkim719 <0000kgy@naver.com>
2024-10-20 13:44:09 +0900

    [Add] Item Manager

---
+++ RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Item/ItemManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    private ItemData itemData;
+
+    private void Awake()
+    {
+        itemData = GetComponent<ItemData>();
+    }
+    public void ItemLife()
+    {
+        
+    }
+    public void ItemBomb()
+    {
+        
+    }
+    public void ItemScore()
+    {
+
+    }
+}
