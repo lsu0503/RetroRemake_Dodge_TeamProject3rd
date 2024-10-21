96ae3a4883152bfeef11c5c32d235dd468ad0123
gwanyoungkim719 <0000kgy@naver.com>
2024-10-21 11:30:09 +0900

    [Update] Leader Board UI

--- RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/LeaderBoardUI/LeaderBoardUI.cs
+++ RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/LeaderBoardUI/LeaderBoardUI.cs
@@ -1,8 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class LeaderBoardUI : MonoBehaviour
 {
+   
+}
+
+public class SoloRankUI : LeaderBoardUI
+{
+    [SerializeField] private TextMeshProUGUI[] SoloPlayerTextList;
     
+    private void SoloSaveScore(int totalScore)
+    {
+       
+    }
+}
+public class MultiRankUI : LeaderBoardUI
+{
+    [SerializeField] private List<TextMeshProUGUI> MultiPlayerTextList;
+    private void MultiSaveScore(int totalScore)
+    {
+
+    }
 }
