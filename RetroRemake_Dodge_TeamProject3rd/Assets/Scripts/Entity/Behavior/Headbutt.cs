d3a41d5e0ae6fdb7b7914ea76ca76477fd7f0446
이선욱(Unity_6기) <lsu0503@gmail.com>
2024-10-22 04:26:27 +0900

    [Add] Monster's Headbutt System

---
+++ RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/Entity/Behavior/Headbutt.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Headbutt : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("HitPoint"))
+        {
+            collision.gameObject.GetComponent<HitPoint>().OnHit(null);
+        }
+    }
+}
