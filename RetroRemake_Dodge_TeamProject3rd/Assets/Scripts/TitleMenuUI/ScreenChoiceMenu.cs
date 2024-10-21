065c9f43a3a4b45a24232a6c8daa835167c4353f
limchaneeee <hypercube8878@gmail.com>
2024-10-22 02:36:25 +0900

    (Update)DifficultyMenu,ScreenChoiceMenu

--- RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/TitleMenuUI/ScreenChoiceMenu.cs
+++ RetroRemake_Dodge_TeamProject3rd/Assets/Scripts/TitleMenuUI/ScreenChoiceMenu.cs
@@ -1,59 +1,41 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScreenModeManager : MonoBehaviour
+public class ScreenChoiceMenu : MonoBehaviour
 {
-   
-    [SerializeField] private GameObject windowModeIndicator;   
-    [SerializeField] private GameObject fullscreenModeIndicator;
-
-    private void Awake()
-    {
-        Screen.fullScreen = true;
-    }
+    public GameObject windowModeIndicator;
+    public GameObject fullscreenModeIndicator;
 
     private void Start()
     {
-        UpdateIndicators();
+        SetFullScreenMode();
     }
 
-    // 창 모드로 전환
+    public void SetFullScreenMode()
+    {
+        Screen.fullScreen = true;
+        Screen.SetResolution(1920, 1080, true);
+        SetTriangle();
+    }
+
     public void SetWindowMode()
     {
-        if (!Screen.fullScreen)
-        {
-            return;
-        }
-
-        Screen.fullScreen = false; 
-        UpdateIndicators(); 
-        Debug.Log("창모드로 변경되었습니다.");
+        Screen.fullScreen = false;
+        SetTriangle();
     }
 
-    // 풀스크린 모드로 전환
-    public void SetFullscreenMode()
-    {
-        if (Screen.fullScreen) 
-        {
-            return;
-        }
-
-        Screen.fullScreen = true; 
-        UpdateIndicators();
-        Debug.Log("풀스크린 모드로 변경되었습니다.");
-    }
-
-    private void UpdateIndicators()
+    private void SetTriangle()
     {
         if (Screen.fullScreen)
         {
-            windowModeIndicator.SetActive(false);  
-            fullscreenModeIndicator.SetActive(true); 
+            fullscreenModeIndicator.SetActive(false);
+            windowModeIndicator.SetActive(true);
         }
         else
         {
-            windowModeIndicator.SetActive(true);  
-            fullscreenModeIndicator.SetActive(false); 
+            fullscreenModeIndicator.SetActive(true);
+            windowModeIndicator.SetActive(false);
         }
     }
+
 }
